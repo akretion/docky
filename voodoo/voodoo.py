91d207a693f7d84e96c4cbed45e9b60ccdfe158f
Raphael Valyi <rvalyi@akretion.com>
2015-10-21 17:01:13 -0200

    commented out buggy remote cleanup command

--- voodoo/voodoo.py
+++ voodoo/voodoo.py
@@ -279,7 +279,8 @@ class VoodooCommand(TopLevelCommand):
         Usage: new [options] PROJECT
         """
         check_call(["git", "clone", TEMPLATE, options['PROJECT']])
-        check_call(["git", "remote", "remove", "origin"])
+# TODO the following may work if we deal with CWD
+#        check_call(["git", "remote", "remove", "origin"]) 
 
     def open(self, project, options):
         """
