c76d86f5113bc44e448cb9e7f5df322bc3f40284
Sébastien BEAU <sebastien.beau@akretion.com>
2016-10-04 00:09:50 +0200

    [BUMP] bump version 2.1.0

--- voodoo/main.py
+++ voodoo/main.py
@@ -14,7 +14,7 @@ import yaml
 from .hook import Deploy, GetMainService, InitRunDev, GenerateDevComposeFile
 compose = local['docker-compose']
 
-__version__ = '2.0.2'
+__version__ = '2.1.0'
 
 
 DEFAULT_CONF = {
