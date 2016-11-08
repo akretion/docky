89f1ae9a17eaeaa98f0d95b9550efaaf716ccb1d
Sébastien BEAU <sebastien.beau@akretion.com>
2016-11-08 09:34:35 +0100

    [FIX] fix loading wagon project, should be tested before locomotive

--- voodoo/__init__.py
+++ voodoo/__init__.py
@@ -4,6 +4,6 @@ from .main import Voodoo
 from . import hook
 from .odoo import hook
 from .ruby import hook
-from .locomotive import hook
 from .wagon import hook
+from .locomotive import hook
 from .rails import hook
