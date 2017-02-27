4a969b1822990bfafbb5ba52812d8a1a31b9d00f
Sébastien BEAU <sebastien.beau@akretion.com>
2017-02-27 10:09:33 +0100

    [FIX] fix dump db during migration process

--- voodoo/main.py
+++ voodoo/main.py
@@ -265,7 +265,7 @@ class VoodooMigrate(VoodooSub):
             self._run(git["checkout", version])
             self._run_ak("build")
             self._run_ak("upgrade")
-            self._run_ak("dump", "--force", "migrated_%s.dump" % version)
+            self._run_ak("db", "dump", "--force", "migrated_%s.dump" % version)
             end = datetime.now()
             self._log("Migrate to version %s in %s" % (version, end-start))
         for log in self._logs:
