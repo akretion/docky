9cc3b635d72e8e9331c82709e6c8369167c555b1
Sébastien BEAU <sebastien.beau@akretion.com>
2018-07-20 14:11:19 +0200

    [BUMP] bump version 4.0.2

--- docky/cmd/base.py
+++ docky/cmd/base.py
@@ -18,7 +18,7 @@ from ..common.proxy import Proxy
 
 class Docky(cli.Application):
     PROGNAME = "docky"
-    VERSION = '4.0.1'
+    VERSION = '4.0.2'
     SUBCOMMAND_HELPMSG = None
 
     dryrun = cli.Flag(
