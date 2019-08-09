45f8887b75bd70114ac0e3a4311e560c7e137be0
hparfr <raph@hpar.fr>
2019-08-09 15:47:43 +0200

    Refactore to use docker-compose project

--- docky/common/project.py
+++ docky/common/project.py
@@ -7,6 +7,7 @@ from compose.cli.command import get_project
 from compose.project import OneOffFilter
 from plumbum import local
 from plumbum.cli.terminal import ask
+from compose.cli import command
 import docker
 import yaml
 import os
@@ -24,23 +25,30 @@ class Project(object):
     def __init__(self, env, docky_config):
         self.env = env
         self.docky_config = docky_config
-        self.compose_file_path = self._get_config_path()
+        self.compose_file_path = self._get_config_path(self.env)
         if self.env == 'dev':
             if not local.path(self.compose_file_path).isfile():
                 self._generate_dev_docker_compose_file()
-        self.name = self._get_project_name()
+        self.project = command.project_from_options(
+            '.', {'--file': [self.compose_file_path]})
+        self.name = self._get_project_name(self.project)
         self.loaded_config = None
-        self.service = self._get_main_service()
+        self.service = self._get_main_service(self.project)
 
-    def _get_main_service(self):
-        for name, service in self.config['services'].items():
-            for label, value in service.get('labels', {}).items():
-                if label == 'docky.main.service' and value is True:
-                    return name
+    def _get_main_service(self, project):
+        """main_service has docky.main.service defined in
+        his label."""
+        for service in project.services:
+            labels = service.options.get('labels', {})
+            # service.labels() do not contain docky.main.service
+            # see also compose.service.merge_labels
+            if labels.get('docky.main.service', False):
+                return service.name
 
-    def _get_config_path(self):
-        config_path = '.'.join([self.env, DOCKER_COMPOSE_PATH])
-        if self.env == 'dev':
+    def _get_config_path(self, env):
+        # TODO a virer et utiliser --files de compose
+        config_path = '.'.join([env, DOCKER_COMPOSE_PATH])
+        if env == 'dev':
             return config_path
         elif local.path(config_path).is_file():
             return config_path
@@ -49,7 +57,7 @@ class Project(object):
         else:
             raise_error(
                 "There is not %s.%s or %s file, please add one"
-                % (self.env, DOCKER_COMPOSE_PATH, DOCKER_COMPOSE_PATH))
+                % (env, DOCKER_COMPOSE_PATH, DOCKER_COMPOSE_PATH))
 
     def _generate_dev_docker_compose_file(self):
         print("There is not dev.docker-compose.yml file.\n")
@@ -61,52 +69,53 @@ class Project(object):
                 return GenerateComposeFile(service).generate()
         raise_error("No dev.docker-compose.yml file, abort!")
 
-    def _get_project_name(self):
+    def _get_project_name(self, project):
+        # TODO return proj.name instead
         return local.env.get(
             'COMPOSE_PROJECT_NAME',
             '%s_%s' % (local.env.user, local.cwd.name)
         )
 
     def get_containers(self, service=None):
-        project = get_project(
-            '.', [self.compose_file_path],
-            project_name=self.name)
         kwargs = {'one_off': OneOffFilter.include}
         if service:
             kwargs['service_names'] = [service]
-        return project.containers(**kwargs)
+        return self.project.containers(**kwargs)
 
     @property
     def config(self):
+        # TODO avirer !
         if not self.loaded_config:
             self.loaded_config = yaml.safe_load(
                 open(self.compose_file_path, 'r'))
         return self.loaded_config
 
     def show_access_url(self):
-        for name, service in self.config['services'].items():
-            for label, value in service.get('labels', {}).items():
-                if label == 'docky.access.help':
-                    logger.info(
-                        "The service %s is accessible on %s"
-                        % (name, value))
+        for service in self.project.services:
+            labels = service.options.get('labels', {})
+            url = labels.get('docky.access.help', False)
+            if url:
+                logger.info(
+                    "The service %s is accessible on %s"
+                    % (service.name, url))
 
     def create_volume(self):
-        for name, service in self.config['services'].items():
-            if 'volumes' in service:
-                for volume_path in service['volumes']:
-                    volume = volume_path.split(':')[0]
-                    if any([volume.startswith(c) for c in ['.', '/', '$']]):
-                        path = local.path(local.env.expand(volume))
-                        if not path.exists():
-                            logger.info(
-                                "Create missing directory %s for service %s",
-                                path, name)
-                            path.mkdir()
+        """Mkdir volumes if they don't exist yet.
 
-    def get_user(self, service):
-        for label, value in \
-                self.config['services'][service].get('labels', {}).items():
-            if label == 'docky.user':
-                return value
-        return None
+        Only apply to external volumes.
+        docker-compose up do not attemps to create it
+        so we have to do it ourselves"""
+        for service in self.project.services:
+            for volume in service.options.get('volumes', []):
+                if volume.external:
+                    path = local.path(local.env.expand(volume.external))
+                    if not path.exists():
+                        logger.info(
+                            "Create missing directory %s for service %s",
+                            path, service.name)
+                        path.mkdir()
+
+    def get_user(self, service_name):
+        service = self.project.get_service(name=service_name)
+        labels = service.options.get('labels')
+        return labels.get('docky.user', None)
