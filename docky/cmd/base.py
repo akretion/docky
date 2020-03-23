cc68118fea29e5e881b17eebfa211bad1f88ba2e
hparfr <raph@hpar.fr>
2020-03-23 14:54:05 +0100

    Bump version to 7.0.3

--- docky/cmd/base.py
+++ docky/cmd/base.py
@@ -15,7 +15,7 @@ from ..common.project import Project
 
 class Docky(cli.Application):
     PROGNAME = "docky"
-    VERSION = '7.0.2'
+    VERSION = '7.0.3'
     SUBCOMMAND_HELPMSG = None
 
     def _run(self, cmd, retcode=FG):
