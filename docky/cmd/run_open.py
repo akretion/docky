f4edc9a7a17d59bd9a426e66475f8b033c85899e
Magno Costa <magno.costa@akretion.com.br>
2024-06-07 14:56:14 -0300

    [IMP] Included new command 'docky system' just return a resume of 'docker info' for check errors related to OS or older or newer libraries versions.

--- docky/cmd/run_open.py
+++ docky/cmd/run_open.py
@@ -79,3 +79,27 @@ class DockyOpen(DockyExec):
     def _main(self, *optionnal_command_line):
         super()._main(*optionnal_command_line)
         self._exec("dcpatched", ["exec", "-e", "NOGOSU=True", self.service] + self.cmd)
+
+@Docky.subcommand("system")
+class DockySystem(DockyExec):
+    """
+    Check your System Infos:
+    OS Type, Kernel, OS, Docker, Docker Compose, and Docky versions.
+    """
+    def _main(self):
+        # Info
+        infos = docker.system.info()
+        # OS Type
+        logger.info("OS Type " + infos.os_type)
+        # Kernel Version
+        logger.info("Kernel Version " + infos.kernel_version)
+        # Operation System
+        logger.info("OS " + infos.operating_system)
+        # Python Version
+        logger.info("Python Version " + sys.version)
+        # Docker Version
+        logger.info("Docker Version " + infos.server_version)
+        # Docker Compose Version
+        logger.info(docker.compose.version())
+        # Docky Version
+        logger.info("Docky Version " + Docky.VERSION)
